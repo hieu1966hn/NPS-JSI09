dbf285ac6d28d4108aa8b5b761d013ea992acfa4
hieu1966hn <hieu1966hn@gmail.com>
2023-02-26 19:43:59 +0700

    new code SS1 Web intensive

---
+++ SS1/SS1-lythuyet.md
@@ -0,0 +1,20 @@
+Gv phụ trách lớp: Nguyễn Trung Hiếu
+Anh trợ giảng: Anh Tú
+Cô quản lý lớp: Vân Anh (Cần hỗ trợ gì ới cô)
+
+1. Phúc Hưng: lớp 12 - Gymer
+2. Trần Đức Anh: Lớp 9 - Tre Trau
+3. Đình Khang: Lớp 11 - Movie
+4. Ngọc Minh: Lớp 12 - Ngủ
+5. Đức Minh: Lớp 10 - Game
+6. Hoàng Minh: Lớp 9 - Game (Roblox)
+7. Nam Hải: Lớp 8 - Đá bóng
+
+Khóa học lập trình Web Intensive
+
+- Chuyên sâu về JS giải thuật: not much
+- Làm bot chat AI: ....tính năng chat =>
+- API
+- DOM chuyên sâu => Cách truy vấn theo MVC
+- Responsive website
+- Database online: noSQL (SQL & noSQL) (firebase, mongo, )
---
+++ SS1/app.js
@@ -0,0 +1,80 @@
+/*
+1. Ôn tập lại về JS
+2. Mô tả về ES6 và  các cú pháp cần nhớ
+3. Async & Await:
+4. Luyện tập thực hành
+
+*/
+
+
+///// Để biết được đây có phải là nn lập trình hay không? Tính logic
+/*
+Biến: Biến là gì? biến có công dụng gì?
+- Biến là nơi lưu trữ giá trị.
+- Biến dùng để lưu trữ giá trị.
+
+Cú pháp: let, var, const
+- let: Biến cục bộ (nên dùng) => ít bị lỗi liên quan tới scope hoạt
+- var: Biến toàn cục
+- const: hằng
+
+
+
+Kiểu dữ liệu cơ bản: boolean, string, number, undefined, null, Object
+
+
+Câu điều kiện: if, if else, if - else if - else
+
+Loop: for, while, do while (for in, for of)
+- while: ktra đk => lặp
+- do while: lặp lần 1 rồi ktra đk
+- Trường hợp dùng với mảng
++ for in: key là vị trí phần tử mảng
++ for of: key là từng phần tử mảng được duyệt
+
+
+Array:
+- cú pháp khai báo mảng:
++ let arr = [  ]; // Array
++ let arr = new Arrray();
+
+- Các hàm CRUD với mảng:
++ arr.push(): Thêm ? phần tử vào cuối mảng
++ arr.pop() : Thêm ? phần tử vào đầu mảng
++ arr.shift() : Xóa 1 phần tử ở đầu mảng
++ arr.unShift() : Xóa 1 phần tử ở cuối mảng
++ arr.splice(box1, box2, box3): vị trí phần tử, xóa ? phần tử từ vị trí đó , thêm ? phần tử từ vị trí đó
+
+
+Object
+- cú pháp: let obj = {
+  "key": value;
+}
+
+ */
+// {
+//   {
+//     {
+//       let i = "Đây là biến i"
+//       var j = "Đây là biến j"
+//     }
+//   }
+// }
+
+// // console.log(i); //?
+// console.log(j); //?
+
+// Vd với for in
+// let arr = [1, 3, 4, 5, 76, 7, 8]; // in ra toàn bộ phần tử mảng với for
+// for (let key in arr) {
+//   console.log(key);
+// }
+// Vd với for of
+// let arr = [1, 3, 4, 5, 76, 7, 8]; // in ra toàn bộ phần tử mảng với for
+// for (let key of arr) {
+//   console.log(key);
+// }
+
+
+
+
