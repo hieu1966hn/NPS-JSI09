54abc4ccbe67d55a60c2e11d6748eeb0e4a503e3
hieu1966hn <hieu1966hn@gmail.com>
2023-03-19 20:34:34 +0700

    done view.js

--- SS4/js/view.js
+++ SS4/js/view.js
@@ -1,1 +1,18 @@
 //// Dùng để chuyển sang các màn hình khác: Trang đăng ký/ đăng nhập/ trang chủ, ..
+const view = {} // Dùng để hiển thị lên màn hình giao diện cho người dùng 
+
+view.setActiveScreen = (screenName) => {
+  switch (screenName) {
+    case 'navbar':
+
+      break;
+    case "registerScreen":
+      break;
+
+    case "loginScreen":
+      break;
+
+    case 'homeScreen':
+      break;
+  }
+}
