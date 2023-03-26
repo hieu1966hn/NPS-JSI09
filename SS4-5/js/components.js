36a93e35e162190a1c525f624ea0f2bb0a5df472
hieu1966hn <hieu1966hn@gmail.com>
2023-03-26 20:44:30 +0700

    Done component login

--- SS4-5/js/components.js
+++ SS4-5/js/components.js
@@ -6,7 +6,7 @@ components.navbar = `
 <h1>Đây là thẻ h1 thứ 2</h1>
 `;
 
-components.registerScreen= `
+components.registerScreen = `
 <div class="register-container">
     <div class="aside-right">
       <div class="header">
@@ -43,6 +43,35 @@ components.registerScreen= `
         <div class="form-action">
           <span id="redirect-to-login">Already have an account? Login</span>
           <button class="btn" type="submit">Register</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+`
+components.login = `
+<div class="register-container">
+    <div class="aside-right">
+      <div class="header">
+        <h3>MindX Chat</h3>
+      </div>
+
+      <form id="register-form">
+
+
+        <div class="input-wrapper">
+          <input type="email" name="email" placeholder="Email">
+          <div class="error" id="email-error"></div>
+        </div>
+
+        <div class="input-wrapper">
+          <input type="password" name="password" placeholder="Password">
+          <div class="error" id="password-error"></div>
+        </div>
+
+        <div class="form-action">
+          <span class="cursor" id="redirect-to-register">Don't have an account? Register</span>
+          <button class="btn" type="submit">Login</button>
         </div>
 
       </form>
