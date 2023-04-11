42c3ab33f3bbf5e2e67346d678d2766307d3502d
hieu1966hn <hieu1966hn@gmail.com>
2023-04-11 14:41:48 +0700

    NEW CODE SS7

--- SS7/script.js
+++ SS7/script.js
@@ -2,6 +2,7 @@ import {
   addDoc,
   collection,
   deleteDoc,
+  doc,
   onSnapshot,
   updateDoc,
 } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
@@ -12,45 +13,43 @@ const displayData = (data) => {
   document.querySelector(".todos").innerHTML = "";
   let output = "";
   for (const item of data) {
-    output += `
-    <div class = "todo${item.checked ? " checked" : ""}">
-      <div>
-        <input data-id="${item.id}" type="checkbox" ${item.checked ? "checked" : ""} />
-        <span>${item.content}</span>
-      </div>
-      <button class="btn remove-btn" data-id="${item.id}">x</button> 
-    `
+    output += /*html*/ `
+        <div class="todo${item.checked ? " checked" : ""}">
+          <div>
+            <input data-id="${item.id}" type="checkbox" ${item.checked ? "checked" : ""
+      } />
+            <span>${item.content}</span>
+          </div>
+          <button class="btn remove-btn" data-id="${item.id}">x</button>
+        </div>
+      `;
   }
+  document.querySelector(".todos").innerHTML = output;
+  document.querySelectorAll(".remove-btn").forEach((btn) => {
+    btn.addEventListener("click", () =>
+      removeTodo(btn.getAttribute("data-id"))
+    );
+  });
+  document.querySelectorAll("input[type=checkbox]").forEach((input) => {
+    input.addEventListener("change", () => {
+      toggleChecked(input.getAttribute("data-id"), input.checked);
+    });
+  });
 };
-
-document.querySelector(".todos").innerHTML = output;
-document.querySelector(".remove-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    removeTodo(btn.getAttribute("data-id"))
-  });
-});
-
-document.querySelectorAll("input[type=checkbox]").forEach((input) => {
-  input.addEventListener("change", () => {
-    toggleChecked(input.getAttribute("data-id"), input.checked);
-  });
-});
-
 
 const removeTodo = (id) => {
   deleteDoc(doc(db, "todos", id));
 };
-
 const toggleChecked = (id, newValue) => {
   updateDoc(doc(db, "todos", id), {
     checked: newValue,
   });
 };
 
-document.querySelector("form").addEventListener("submit", async (event) => {
-  event.preventDefault();
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-  const inputValue = document.querySelector("input[type=text]").value = "";
+  const inputValue = document.querySelector("input[type=text]").value.trim();
 
   if (inputValue.trim()) {
     document.querySelector("input[type=text]").value = "";
@@ -58,9 +57,14 @@ document.querySelector("form").addEventListener("submit", async (event) => {
     await addDoc(collection(db, "todos"), {
       content: inputValue,
       checked: false,
-    })
+    });
   }
 });
 
-
-
+onSnapshot(collection(db, "todos"), (querySnapshot) => {
+  const todos = [];
+  querySnapshot.forEach((doc) => {
+    todos.push({ ...doc.data(), id: doc.id });
+  });
+  displayData(todos);
+});
