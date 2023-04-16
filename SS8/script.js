6bbf80e83a1d7b94c3aa2090efdf6d6c7a687099
hieu1966hn <hieu1966hn@gmail.com>
2023-04-16 19:49:15 +0700

    New code create collection, document

---
+++ SS8/script.js
@@ -0,0 +1,22 @@
+// const users = {
+//   "O7edpHqSjwJUQ1j3CsIX": {
+//     email: "hieu1966hn@gmail.com",
+//     firstName: "Hiếu"
+//   },
+
+// }
+
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  deleteField,
+  doc,
+  setDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
+
+import { db } from "./firebase.js";
+
+const cityRef = doc(db, 'cities', 'HN');
+setDoc(cityRef, { capital: true }, { merge: true });
