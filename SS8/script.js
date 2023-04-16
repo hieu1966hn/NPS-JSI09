42c775e28d77ef5a47a8044e68f3e58403bde30f
hieu1966hn <hieu1966hn@gmail.com>
2023-04-16 20:21:16 +0700

    New code Thêm mới HCM

--- SS8/script.js
+++ SS8/script.js
@@ -29,3 +29,24 @@ await updateDoc(cityRef, {
   air: "Ô nhiễm",
   drinks: ["beer", "mixue", "HighLands"]
 })
+
+
+/// Thêm mới document HCM => Trường dữ liệu tương tự như là HN 
+await setDoc(doc(db, 'cities', 'HCM'), {
+  range: "Ghi gì cũng được",
+  traffic: "traffic jam",
+  food: "Bún trộn Nam Bộ",
+  air: "Ô nhiễm",
+  drinks: ["Beer"]
+});
+/*
+Nhưng đôi khi không có ID có ý nghĩa cho tài liệu và sẽ thuận tiện hơn
+ nếu để Cloud Firestore tự động tạo ID cho bạn.
+ Bạn có thể thực hiện việc này bằng cách gọi các phương thức add()
+ dành riêng cho ngôn ngữ sau:
+*/
+// await addDoc(collection(db, "cities"), {
+//   name: "Hà Nội",
+//   country: "Việt Nam"
+// });
+// console.log("Document written with ID: ", cityRef.id);
